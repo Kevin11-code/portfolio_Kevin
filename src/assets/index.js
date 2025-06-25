b299b7b50994cedd171755e627f15f476e7bc7ad
kevin11-code <kevinharia11@gmail.com>
2025-06-26 00:46:31 +0530

    Add new assets and update project details
    - Added new company logos for ARCON and VENQ.
    - Updated index.js to include new tech icons: C++, Java, Python, Linux, and Bash.
    - Modified Experience component to enhance image styling.
    - Changed Works component export to "projects".
    - Updated constants to reflect new technologies and services.
    - Added new experiences for ARCON and VENQ.
    - Updated project descriptions and added new projects: Filepilot, ChatAI, DecentSupply, Validize, and ExpenseTracker.

--- src/assets/index.js
+++ src/assets/index.js
@@ -9,23 +9,26 @@ import close from "./close.svg";
 
 import css from "./tech/css.png";
 import docker from "./tech/docker.png";
-import figma from "./tech/figma.png";
+import cpp from "./tech/cpp.png";
 import git from "./tech/git.png";
 import html from "./tech/html.png";
 import javascript from "./tech/javascript.png";
 import mongodb from "./tech/mongodb.png";
 import nodejs from "./tech/nodejs.png";
 import reactjs from "./tech/reactjs.png";
-import redux from "./tech/redux.png";
+import java from "./tech/java.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 import nextjs from "./tech/nextjs.png";
+import python from "./tech/python.png";
+import linux from "./tech/linux.png";
+import bash from "./tech/bash.png";
 
 import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
+import arcon from "./company/arcon.jpg";
 import graphity from "./company/graphity.png";
-import tesla from "./company/tesla.png";
+import venq from "./company/venq.jpg";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
@@ -42,23 +45,26 @@ export {
   close,
   css,
   docker,
-  figma,
+  cpp,
   git,
   html,
   javascript,
   mongodb,
   nodejs,
   reactjs,
-  redux,
+  java,
   tailwind,
   typescript,
   nextjs,
   threejs,
   meta,
-  shopify,
+  arcon,
   graphity,
-  tesla,
+  venq,
   carrent,
   jobit,
   tripguide,
+  python,
+  linux,
+  bash
 };
--- src/constants/index.js
+++ src/constants/index.js
@@ -8,22 +8,25 @@ import {
   html,
   css,
   reactjs,
-  redux,
+  java,
   tailwind,
   nodejs,
   mongodb,
   git,
-  figma,
+  cpp,
   docker,
   meta,
   graphity,
-  tesla,
-  shopify,
+  venq,
+  arcon,
   carrent,
   jobit,
   tripguide,
   threejs,
   nextjs,
+  linux,
+  python,
+  bash
 } from "../assets";
 
 export const navLinks = [
@@ -36,6 +39,10 @@ export const navLinks = [
     title: "Work",
   },
   {
+    id: "projects",
+    title: "Projects",
+  },
+  {
     id: "contact",
     title: "Contact",
   },
@@ -46,10 +53,10 @@ const services = [
     title: "Frontend Web Developer",
     icon: web,
   },
-  // {
-  //   title: "React Native Developer",
-  //   icon: mobile,
-  // },
+  {
+    title: "React Native Developer",
+    icon: mobile,
+  },
   {
     title: "Backend Developer",
     icon: backend,
@@ -61,6 +68,26 @@ const services = [
 ];
 
 const technologies = [
+  {
+    name: "Python",
+    icon: python,
+  },
+  {
+    name: "Linux",
+    icon: linux,
+  },
+  {
+    name: "Bash",
+    icon: bash,
+  },
+  {
+    name: "cpp",
+    icon: cpp,
+  },
+  {
+    name: "Java",
+    icon: java,
+  },
   {
     name: "HTML 5",
     icon: html,
@@ -81,10 +108,7 @@ const technologies = [
     name: "React JS",
     icon: reactjs,
   },
-  {
-    name: "Redux Toolkit",
-    icon: redux,
-  },
+
   {
     name: "Next JS",
     icon: nextjs,
@@ -109,10 +133,7 @@ const technologies = [
   //   name: "git",
   //   icon: git,
   // },
-  {
-    name: "figma",
-    icon: figma,
-  },
+
   // {
   //   name: "docker",
   //   icon: docker,
@@ -133,32 +154,33 @@ const experiences = [
       "Demonstrated proficiency in using Nextjs and Tailwind CSS to create visually appealing and responsive web pages.",
     ],
   },
-  // {
-  //   title: "React Native Developer",
-  //   company_name: "Tesla",
-  //   icon: tesla,
-  //   iconBg: "#E6DEDD",
-  //   date: "Jan 2021 - Feb 2022",
-  //   points: [
-  //     "Developing and maintaining web applications using React.js and other related technologies.",
-  //     "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-  //     "Implementing responsive design and ensuring cross-browser compatibility.",
-  //     "Participating in code reviews and providing constructive feedback to other developers.",
-  //   ],
-  // },
-  // {
-  //   title: "Web Developer",
-  //   company_name: "Shopify",
-  //   icon: shopify,
-  //   iconBg: "#383E56",
-  //   date: "Jan 2022 - Jan 2023",
-  //   points: [
-  //     "Developing and maintaining web applications using React.js and other related technologies.",
-  //     "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-  //     "Implementing responsive design and ensuring cross-browser compatibility.",
-  //     "Participating in code reviews and providing constructive feedback to other developers.",
-  //   ],
-  // },
+  {
+    title: "Full Stack Developer",
+    company_name: "VENQ",
+    icon: venq,
+    iconBg: "#E6DEDD",
+    date: "May 2023 - Jun 2023",
+    points: [
+      "Collaborated with the external SabPaisa team to integrate their payment gateway into Fullcover, enabling secure and efficient online ticket purchases.",
+      "Built and maintained the backend of Fullcover using Node.js and Express.js, with MongoDB for data storage and NextAuth.js for user authentication and session handling.",
+      "Ensured robust API architecture and payment workflows that supported the complete club ticketing lifecycle.",
+      "Developed the frontend for PostPal, a web platform for hiring influencers, using Next.js and Tailwind CSS for fast, responsive, and modern UI design.",
+      "Focused on delivering a seamless and accessible user experience in PostPal for both brands and influencers, with clean design and optimized performance.",
+    ],
+  },
+  {
+    title: "Software Developer",
+    company_name: "ARCON",
+    icon: arcon,
+    iconBg: "#383E56",
+    date: "Jul 2024 - Present",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
   // {
   //   title: "Full stack Developer",
   //   company_name: "Meta",
@@ -203,28 +225,28 @@ const testimonials = [
 
 const projects = [
   {
-    name: "Car Rent",
+    name: "Filepilot",
     description:
       "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
     tags: [
       {
-        name: "react",
+        name: "python",
         color: "blue-text-gradient",
       },
       {
-        name: "mongodb",
+        name: "pyqt5",
         color: "green-text-gradient",
       },
       {
-        name: "tailwind",
+        name: "paramiko",
         color: "pink-text-gradient",
       },
     ],
     image: carrent,
-    source_code_link: "https://github.com/",
+    source_code_link: "https://github.com/Kevin11-code/filepilot",
   },
   {
-    name: "Job IT",
+    name: "ChatAI",
     description:
       "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
     tags: [
@@ -245,7 +267,70 @@ const projects = [
     source_code_link: "https://github.com/",
   },
   {
-    name: "Trip Guide",
+    name: "DecentSupply.",
+    description:
+      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Validize",
+    description:
+      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "ExpenseTracker",
+    description:
+      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "DecentSupply.",
     description:
       "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
     tags: [
